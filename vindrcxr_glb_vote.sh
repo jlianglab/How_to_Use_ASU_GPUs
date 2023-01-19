fb57d18c4db4c4351dc1fa3a8de70c22942121f9
MRJasonP <39168364+MRJasonP@users.noreply.github.com>
2023-01-19 14:33:03 -0700

    Delete vindrcxr_glb_vote.sh

--- vindrcxr_glb_vote.sh
+++
@@ -1,9 +0,0 @@
-python vindrcxr.py --gpu 0,1 --gt global_vote --run 1 --batch_size 200
-
-python vindrcxr.py --gpu 0,1 --gt global_vote --run 2 --batch_size 200
-
-python vindrcxr.py --gpu 0,1 --gt global_vote --run 3 --batch_size 200
-
-python vindrcxr.py --gpu 0,1 --gt global_vote --run 4 --batch_size 200
-
-python vindrcxr.py --gpu 0,1 --gt global_vote --run 5 --batch_size 200
