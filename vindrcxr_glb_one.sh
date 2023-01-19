62da3c898d7fe25b2e88c18983171a21b8b82d3e
MRJasonP <39168364+MRJasonP@users.noreply.github.com>
2023-01-19 14:33:10 -0700

    Delete vindrcxr_glb_one.sh

--- vindrcxr_glb_one.sh
+++
@@ -1,5 +0,0 @@
-python vindrcxr.py --gpu 2,3 --gt global_one --run 1 --batch_size 200
-python vindrcxr.py --gpu 2,3 --gt global_one --run 2 --batch_size 200
-python vindrcxr.py --gpu 2,3 --gt global_one --run 3 --batch_size 200
-python vindrcxr.py --gpu 2,3 --gt global_one --run 4 --batch_size 200
-python vindrcxr.py --gpu 2,3 --gt global_one --run 5 --batch_size 200
